be43796cdf6cb6b263761da6a99acc7969928513
PriyadharshiniMillar <priya190196@gmail.com>
2016-08-13 12:51:29 +0530

    Add files via upload

---
+++ numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+int main(void) 
+{
+	// your code goes here
+	int n;
+
+	scanf("%d",&n);
+	
+if(n>=1)
+	
+printf("Positive");
+	
+else if(n==0)
+	
+printf("Zero");
+	
+else
+	
+printf("Negative");
+	
+return 0;
+
+}
